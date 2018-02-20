2644eb021253f0cbacb6451241783625b5e9b40b
NyanKiyoshi <NyanKiyoshi@users.noreply.github.com>
2018-02-20 03:55:20 +0100

    bump 0.0.3rc0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     author_email='hello@vanille.bid',
     url='https://github.com/NyanKiyoshi/invoice-generator',
 
-    version='0.0.2',
+    version='0.0.3rc0',
     name=PKG_NAME,
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
